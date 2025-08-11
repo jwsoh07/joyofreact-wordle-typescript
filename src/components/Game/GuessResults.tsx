1594df849902ee6534e16b64439295c17c1eb497
jwsoh07 <mail.junwen@gmail.com>
2025-08-11 23:05:28 +0800

    Update UI to render guesses in presentable cells

--- src/components/Game/GuessResults.tsx
+++ src/components/Game/GuessResults.tsx
@@ -1,17 +1,41 @@
+//import { NUM_OF_GUESSES_ALLOWED } from "../../constants";
+import { NUM_OF_GUESSES_ALLOWED } from "../../constants";
 import type { Word } from "../../data";
+import { range } from "../../utils";
+
+interface LetterProps {
+  letter: string;
+}
+
+interface GuessProps {
+  word: Word;
+} 
 
 interface GuessResultsProps {
   guesses: Word[];
 }
 
+function Letter ({ letter }: LetterProps) {
+  return (
+    <span className="cell">{letter}</span>
+  );
+} 
+
+function Guess( { word }: GuessProps) {
+  const renderedWord = word || "     ";
+  return (
+    <p className="guess">
+      {renderedWord.split("").map((letter, index) => <Letter key={index} letter={letter} />)}
+    </p>
+  );
+}
+
 export default function GuessResults({ guesses }: GuessResultsProps) {
   return (
-    <div className="guess-results">
-      {guesses.map((guess, index) => (
-        <p className="guess" key={index}>
-          {guess}
-        </p>
-      ))}
-    </div>
+      <div className="guess-results">
+        {range(0, NUM_OF_GUESSES_ALLOWED).map((index) => (
+          <Guess key={index} word={guesses[index]} />
+        ))}
+      </div>
   );
 }
